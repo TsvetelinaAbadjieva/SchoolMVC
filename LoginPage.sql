ce560f7ee1d955f646946c1c5a2276f260e52ece
Tsvetelina Abadjieva <tsvetelina_abadjieva@abv.bg>
2017-05-12 13:14:39 +0300

    Add files via upload

---
+++ LoginPage.sql
@@ -0,0 +1,28 @@
+﻿
+	
+begin
+set nocount on;
+	declare @UserId int, @LastLogin datetime
+
+	SELECT @UserId=Id, @LastLogin=LastLogin
+	FROM Users 
+	WHERE UserName='tsvetelina_abadjieva@abv.bg' and [Password]='tsvety'
+	IF @UserId IS NOT NULL
+		BEGIN
+		IF NOT EXISTS (SELECT Id from Users WHERE Id=@UserId)
+			begin
+				UPDATE Users set LastLogin=getdate() where Id=@UserId select @UserId [Id]
+			end
+		ELSE
+			begin
+				select 0--user not activated
+			end
+		END
+	ELSE begin
+				select 2--user is invalid
+		end
+end
+
+declare @PersonID varchar(50)
+select @PersonID=PersonID from users where (PersonID='57')
+update users set Status=1, role='admin', IsActive='1', LastLogin= getdate() where (PersonID='57') 
